850777ecef766bb877eb91e48c7e03636578b146
marynias <mkesobczyk@gmail.com>
2017-02-08 11:05:23 +0000

    Update structure_analysis.sh

--- structure_analysis.sh
+++ structure_analysis.sh
@@ -14,7 +14,7 @@ pgdspid=/home/sobczm/bin/PGDSpider_2.1.0.3
 #haploid (for diploid change the conversion script to vcf_to_structure_diploid.spid)
 #!!!! Need to change the path to file with population definitions !!!
 ##Part of the path is missing here: should it be 
-input_file=$input/SNP_calling/Ash_farm_172_pacbio_contigs_unmasked_filtered_thinned.recode.vcf
+input_file=/SNP_calling/Ash_farm_172_pacbio_contigs_unmasked_filtered_thinned.recode.vcf
 #Prepare population definition file. Each individual = new population
 grep "#CHROM" $input_file | head -1 | awk '{for(i=10;i<=NF;++i)print $i " " $i "_pop"}' >"${input_file%.vcf}.lst"
 #Copy the configuration file and change the path to the population definition file.

8ce4c279522ca8fc526e8e770f2fd7ca88871ec0
passet <tom.passey@emr.ac.uk>
2017-02-09 09:03:35 +0000

    edits to remove dapc and some wording

--- Ash_farm_amova_dapc.R
+++ Ash_farm_amova_dapc.R
@@ -17,24 +17,16 @@ y <- loci2genind(x)
 #Change ploidy to 1 for V. inaequalis data
 ploidy(y) <- 1
 
-#Number of individuals for maximum number of considered clusters
-a <- nInd(y) - 23
-
-#Find the true number of genetic clusters.
-grp <- find.clusters(y, n.pca=200, max.n.clust=a)
-dapc1 <- dapc(y, grp$grp, n.pca=200)
-scatter(dapc1)
-
 
 #AMOVA analysis
 #Note: classification into pathogens, non-pathogens and INTERMEDIATES not more informative than just a binary one
-#Classify according to if pathogenic or not
+#Classify according to cultivar
 other(y)$cultivar <- c("Bramley", "Bramley", "Bramley", "Bramley", "Bramley", "Bramley", "Bramley", "Cox", "Cox", "Cox", "Cox", "Cox", "Cox", "Cox", "Cox", "Worcester", "Worcester", "Worcester", "Worcester", "Worcester", "Worcester", "Worcester", "Worcester")
 
 
 strata_df <- data.frame(other(y))
 strata(y) <- strata_df
 sink(file = "amova.txt", append = T, type = c("output", "message"), split = F)
-amova.pegas <- poppr.amova(y, ~pathogen, method = "pegas")
+amova.pegas <- poppr.amova(y, ~cultivar, method = "pegas")
 amova.pegas
-
+sink()
